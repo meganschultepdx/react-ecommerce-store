16ff87044c620fc03bd30975cbcf71ab0a002e17
megan schulte <megan.schulte.pdx@gmail.com>
2019-12-24 12:39:23 -0800

    add routing to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,15 +7,19 @@ import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Default from "./components/Default";
 import Details from "./components/Details";
+import Cart from "./components/Cart";
 
 class App extends Component {
 	render() {
 		return (
 			<React.Fragment>
 				<Navbar />
-				<ProductList />
-				<Details />
-				<Default />
+				<Switch>
+					<Route exact path="/" component={ProductList} />
+					<Route exact path="/details" component={Details} />
+					<Route exact path="/cart" component={Cart} />
+					<Route component={Default} />
+				</Switch>
 			</React.Fragment>
 		);
 	}
