373bdc60d3cee9524416df4b16c0fcb9a2fbb448
megan schulte <megan.schulte.pdx@gmail.com>
2019-12-24 13:50:00 -0800

    add bootstrap navbar and button component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import erdoben_logo from "../images/erdoben_logo.jpg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export class Navbar extends Component {
 	render() {
 		return (
-			<div>
-				<h3>Hello from Navbar</h3>
-			</div>
+			<nav className="navbar navbar-dark bg-dark">
+				<Link to="/">
+					<img src={erdoben_logo} alt="erdoben logo" className="navbar-brand" />
+				</Link>
+				<ul className="navbar-nav align-items-center">
+					<li className="nav-item ml-5">
+						<Link to="/" className="nav-link">
+							products
+						</Link>
+					</li>
+				</ul>
+				<Link to="/cart" className="ml-auto">
+					<ButtonContainer>
+						<span className="mr-2">
+							<i className="fas fa-cart-plus" />
+						</span>
+						my cart
+					</ButtonContainer>
+				</Link>
+			</nav>
 		);
 	}
 }
